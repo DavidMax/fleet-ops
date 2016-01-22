d871f9ab4d8b66cc53711a80d572a8bcb7a01a50
DavidMax <davidjamesmax@gmail.com>
2016-01-21 22:20:18 -0800

    Create basic home view

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,45 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+        // TODO: Convert this to a service
+		$scope.alerts = [
+            {
+                icon: 'fa-truck fa-2x',
+                color: 'btn-danger',
+                total: '1462',
+                description: 'Trucks'
+            },
+            {
+                icon: 'fa-ship fa-2x',
+                color: 'btn-warning',
+                total: '57',
+                description: 'Ships'
+            },
+            {
+                icon: 'fa-plane fa-2x',
+                color: 'btn-warning',
+                total: '41',
+                description: 'Planes'
+            },
+            {
+                icon: 'fa-train fa-2x',
+                color: 'btn-success',
+                total: '398',
+                description: 'Trains'
+            },
+            {
+                icon: 'fa-wrench fa-2x',
+                color: 'btn-info',
+                total: '12',
+                description: 'Maintenance Requests'
+            },
+            {
+                icon: 'fa-users fa-2x',
+                color: 'btn-info',
+                total: '491',
+                description: 'Operators'
+            }
+		];
 	}
 ]);
